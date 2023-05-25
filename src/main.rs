0ac069a90253bc69b033fc959076028ef2380161
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-05-24 22:15:17 -0700

    tag test

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     board.game.get_jumps(true);
 
     let moves = board::CheckersBitboard::pregen_moves();
-
     println!("Move: {:064b}", moves[1][19]);
     board::CheckersBitboard::pretty_print_bitboard(moves[1][19]);
 
